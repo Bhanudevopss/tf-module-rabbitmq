a9b91f30c3325d59ed14a602dfafa8401035adbd
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-03-28 13:46:29 +0530

    Update tf-module-rabbitmq/

--- vars.tf
+++ vars.tf
@@ -1,4 +1,12 @@
 variable "env" {}
-variable "subnet_ids" {}
 variable "tags" {}
+variable "subnet_ids" {}
 variable "instance_type" {}
+variable "allow_subnets" {}
+variable "vpc_id" {}
+variable "bastion_cidr" {}
+variable "dns_domain" {}
+variable "component" {
+  default = "rabbitmq"
+}
+
